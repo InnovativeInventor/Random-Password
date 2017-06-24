4a2cf6d9f4b5d15a11ef397911f49ce2fd30f76e
Inventor <maxfan8@gmail.com>
2017-06-23 22:42:30 -0400

    Add files via upload

---
+++ gen.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo Welcome to the random password assistor created by Max
+echo This is version 2
+echo "This now uses your Mac's word list at /usr/share/dict/words. It has 235886 entries."
+echo
+echo Please type in the amount of random words that you need
+read number
+echo
+foo=$number
+echo Generating random stuff
+echo Please do some other things on this computer to generate entropy
+
+firstnumber=$(
+LC_ALL=C tr -dc '0-2' </dev/random | head -c 1
+)
+
+if ((firstnumber == 2)); then
+  firstnumberif=$(
+  LC_ALL=C tr -dc '0-3' </dev/random | head -c 1
+  )
+  finishnumberif=$(
+  LC_ALL=C tr -dc '0-9' </dev/random | head -c 4
+  )
+  totalif=$(( firstnumber*100000+firstnumberif*10000+finishnumberif ))
+  echo $totalif "number is two"
+  awk -v "NR=totalif" /usr/share/dict/words > randomwords.txt
+else
+  finishnumberelse=$(
+  LC_ALL=C tr -dc '0-9' </dev/random | head -c 5
+  )
+  totalelse=$(( firstnumber*100000+finishnumberelse ))
+  echo $totalelse "number is not two"
+  awk -v "NR=totalif" /usr/share/dict/words > randomwords.txt
+fi
