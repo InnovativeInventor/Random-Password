bd85cced35f6ba56528899a72ad9c0dbf5278479
InnovativeInventor <InnovativeInventor@gmail.com>
2017-06-24 10:37:35 -0400

    Added progress meter

--- gen.sh
+++ gen.sh
@@ -13,6 +13,7 @@ echo Your output will come out in a file named randomwords.txt in this directory
 echo
 echo Type in the number of random characters that you want after your word
 read char
+echo
 
 COUNTER=$foo
 until [ $COUNTER -lt 1 ]; do
@@ -30,7 +31,6 @@ until [ $COUNTER -lt 1 ]; do
     )
     finalnumberif=$(echo $finishnumberif | sed 's/^0*//')
     totalif=$(( firstnumber*100000+firstnumberif*10000+finalnumberif ))
-    echo $totalif
     word=`sed -n "$totalif p" /usr/share/dict/words`
     randomsymbols=$(LC_ALL=C tr -dc '0-9!@#$%^&*()_+-=<>/?.' </dev/random | head -c $char)
     echo $word$randomsymbols >> randomwords.txt
@@ -40,11 +40,14 @@ until [ $COUNTER -lt 1 ]; do
     )
     finalnumberelse=$(echo $finishnumberelse | sed 's/^0*//')
     totalelse=$(( firstnumber*100000+finalnumberelse ))
-    echo $totalelse
     word=`sed -n "$totalelse p" /usr/share/dict/words`
     randomsymbols=$(LC_ALL=C tr -dc '0-9!@#$%^&*()_+-=<>/?.' </dev/random | head -c $char)
     echo $word$randomsymbols >> randomwords.txt
   fi
   let COUNTER-=1
-  
+  progress=$((number-COUNTER))
+  echo Progress: "$progress/$number\r\c"
 done
+echo
+echo "Done! Check randomwords.txt."
+echo
