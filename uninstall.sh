5caaa21652a98867709c7ac99b56ca8de2f745ce
InnovativeInventor <InnovativeInventor@gmail.com>
2017-06-25 09:27:57 -0400

    Updates

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo Uninstalling
-rm -r /usr/local/opt/rp
+sudo rm -r /usr/local/opt/rp
 
 echo Deleting myself . . .
 echo 'To reinstall, go to https://github.com/InnovativeInventor/Random-Password'
 echo Byebye!
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rm -r $DIR
+sudo rm -r $DIR
