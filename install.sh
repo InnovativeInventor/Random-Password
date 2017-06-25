8350054aba4010050c44370e84ef68ff080869da
InnovativeInventor <InnovativeInventor@gmail.com>
2017-06-25 12:28:13 -0400

    Getting rid of .sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ sudo mkdir /usr/local/opt/rp
 echo Downloading
 git clone git://github.com/InnovativeInventor/Random-Password
 sudo cp -r ~/Random-Password/bin /usr/local/opt/rp/
-chmod +x /usr/local/opt/rp/rpsafe
-chmod +x /usr/local/opt/rp/rp
+chmod +x /usr/local/opt/rp/bin/rpsafe
+chmod +x /usr/local/opt/rp/bin/rp
 echo ' ' >> ~/.bash_profile
 echo '# Setting PATH for random password generator' >> ~/.bash_profile
 echo 'export PATH="/usr/local/opt/rp/bin:$PATH"' >> ~/.bash_profile
