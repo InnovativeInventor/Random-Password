86a0e74e3a29cefac5901d43f7079192af32b503
InnovativeInventor <InnovativeInventor@gmail.com>
2017-07-01 08:18:56 -0400

    Added comments to install and uninstall

--- install.sh
+++ install.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 echo Installing
+# Making directory on computer to move files to
 sudo mkdir /usr/local/opt/rp
-echo Downloading
-sudo rm -r ~/Random-Password
-git clone https://github.com/InnovativeInventor/Random-Password --recursive
+
+echo Uninstalling previous versions and downloading
+
+# Deleting previous installs of Random-Password
+sudo sh ~/Random-Password/uninstall.sh > /dev/null
+
+# Recursively cloning from Github
+git clone https://github.com/InnovativeInventor/Random-Password --recursive > /dev/null
+
+# Copying over to the directory created
 sudo cp -r ~/Random-Password/bin /usr/local/opt/rp/
+
+# Moving safedict over to be together with all the scripts
 sudo mv /usr/local/opt/rp/bin/12dict4schools/safedict.txt /usr/local/opt/rp/bin/safedict.txt
+
+# Removing .sh from executables
 sudo mv /usr/local/opt/rp/bin/rpsafe.sh /usr/local/opt/rp/bin/rpsafe
 sudo mv /usr/local/opt/rp/bin/rp.sh /usr/local/opt/rp/bin/rp
 sudo mv /usr/local/opt/rp/bin/rpn.sh /usr/local/opt/rp/bin/rpn
 sudo mv /usr/local/opt/rp/bin/rpc.sh /usr/local/opt/rp/bin/rpc
 sudo mv /usr/local/opt/rp/bin/rpd.sh /usr/local/opt/rp/bin/rpd
-echo Adding executable priviliges
+
+# Adding executable privileges
 sudo chmod +x /usr/local/opt/rp/bin/rpsafe
 sudo chmod +x /usr/local/opt/rp/bin/rp
 sudo chmod +x /usr/local/opt/rp/bin/rpc
 sudo chmod +x /usr/local/opt/rp/bin/rpn
 sudo chmod +x /usr/local/opt/rp/bin/rpd
+
+# Adding to bash profile
 echo ' ' >> ~/.bash_profile
+cp /.bash_profile /.bash_profile.bac
 source ~/.bash_profile
 echo '# Setting PATH for random password generator' >> ~.bash_profile
 echo 'export PATH="/usr/local/opt/rp/bin:$PATH"' >> ~.bash_profile
--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 echo Uninstalling
-sudo rm -r /usr/local/opt/rp
 
+# Removing from .bash_profile
+mv ~/.bash_profile.bac ~/.bash_profile
+
+# Deleting files
+sudo rm -r /usr/local/opt/rp
 echo Deleting myself . . .
 echo 'To reinstall, go to https://github.com/InnovativeInventor/Random-Password'
 echo Byebye!
 sudo rm -r ~/Random-Password
 sudo rm -r ~/randompassword.txt
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sudo rm -r $DIR
+
+# These commands are a bit noisy so I decided to silence them
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" > /dev/null
+sudo rm -r $DIR > /dev/null
